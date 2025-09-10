186753eedd70f8b2ec605ebd396266e3377344e3
rolevax <rolevax@outlook.com>
2025-09-10 13:34:27 +0800

    hide anvil in production build

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,16 +2,13 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { anvil, arbitrum, mainnet, sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: "RainbowKit demo",
-  projectId: "YOUR_PROJECT_ID",
+  appName: "Buy Say Sell",
+  projectId: "497de18cc174f02dec5b52d323c92893",
   chains: [
     mainnet,
     arbitrum,
-    anvil,
     sepolia,
-    // ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
-    //   ? [sepolia, anvil]
-    //   : []),
+    ...(process.env.NODE_ENV === "development" ? [anvil] : []),
   ],
   ssr: true,
 });
